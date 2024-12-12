c944872b8eb0d1e11686c23fb8ddc0a7d5231b08
Karen-404 <tat.3012.mc@gmail.com>
2024-12-12 14:32:37 -0500

    creacion

---
+++ Desktop/practica/prueba/src/main/java/ec/edu/espoch/prueba/Prueba.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.prueba;
+
+/**
+ *
+ * @author SO-LAB1-PC24
+ */
+public class Prueba {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
